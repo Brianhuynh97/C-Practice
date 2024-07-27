6adfe0a4274f296beb2964919eeb9a119147838b
Brian huynh <vuduchv@gmail.com>
2024-07-27 18:02:47 +1000

    editted

--- main.cpp
+++ main.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 
-int main()
-{
-  while (true)
-    {
+void printMenu()
+
+  {
     // 1 print help
     std::cout << "1: Print help " << std::endl;
     // 2 print exchange stats
@@ -14,45 +13,92 @@ int main()
     std::cout << "4: Make a bid " << std::endl;
     // 5 print wallet
     std::cout << "5: Print wallet " << std::endl;
-  // 6 continue
+    // 6 continue
     std::cout << "6: Continue " << std::endl;
 
     std::cout << " ========== " << std::endl;
-    std::cout << " Type in 1-6 " << std::endl;
+  }
 
+void printHelp()
+{
+  std::cout << "Help - your aim is to make money. Analyse the market and make bids and offers" << std::endl;
+}
+
+void printMarketStats()
+{
+  std::cout << "Market looks good" << std::endl;
+}
+
+void enterOffer()
+{
+  std::cout << "Mark and offer - enter the amount" << std::endl;
+}
+
+void enterBid()
+{
+  std::cout << "Make a bid - enter the amount " << std::endl;
+}
+
+void printWallet()
+{
+  std::cout << "Your wallet is empty." << std::endl;
+}
+
+void gotoNextTimeframe()
+{
+  std::cout << "Going to next time frame." << std::endl;
+}
+
+  int getUserOption()
+  {
     int userOption;
+    std::cout << " Type in 1-6 " << std::endl;
     std::cin >> userOption;
-    std::cout <<"You chose: " << userOption << std::endl;
+    std::cout << "You chose: " << userOption << std::endl;
+    return userOption;
+  }
 
-    if(userOption == 0) //bad input
-    {
-      std::cout << "Invalid choice. Choose 1-6" << std::endl;
-    }
-    if (userOption == 1) // bad input
-    {
-      std::cout << "Help - your aim is to make money. Analyse the market and make bids and offers" << std::endl;
-    }
-    if (userOption == 2) // bad input
-    {
-      std::cout << "Market looks good" << std::endl;
-    }
-    if (userOption == 3) // bad input
-    {
-      std::cout << "Mark and offer - enter the amount" << std::endl;
-    }
-    if (userOption == 4) // bad input
-    {
-      std::cout << "Make a bid - enter the amount " << std::endl;
-    }
-    if (userOption == 5) // bad input
-    {
-      std::cout << "Your wallet is empty." << std::endl;
-    }
-    if (userOption == 6) // bad input
-    {
-      std::cout << "Going to next time frame." << std::endl;
-    }
-    }
 
+void processUserOption(int userOption)
+
+{
+  if (userOption == 0) // bad input
+  {
+    std::cout << "Invalid choice. Choose 1-6" << std::endl;
+  }
+  if (userOption == 1) // bad input
+  {
+    printHelp();
+  }
+  if (userOption == 2) // bad input
+  {
+    printMarketStats();
+  }
+  if (userOption == 3) // bad input
+  {
+    enterOffer();
+  }
+  if (userOption == 4) // bad input
+  {
+    enterBid();
+  }
+  if (userOption == 5) // bad input
+  {
+    printWallet();
+  }
+  if (userOption == 6) // bad input
+  {
+    gotoNextTimeframe();
+  }
+}
+
+int main()
+{
+  while (true)
+  {
+      printMenu();
+      int userOption = getUserOption();
+      processUserOption(userOption);
+  }
   return 0;
 }
