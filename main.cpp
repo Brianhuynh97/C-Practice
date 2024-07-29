4706b54b08d139a88705ab19cb55df31f25a7656
Brian huynh <vuduchv@gmail.com>
2024-07-30 00:48:39 +1000

    create vecot of objects

--- main.cpp
+++ main.cpp
@@ -1,4 +1,37 @@
 #include <iostream>
+#include <string>
+#include <vector>
+
+enum class OrderBookType{bid, ask};
+
+class OrderBookEntry
+{
+  public:
+    OrderBookEntry(double _price,
+                   double _amount,
+                   std::string _timestamp,
+                   std::string _product,
+                   OrderBookType _orderType)
+    : price(_price),
+    amount(_amount),
+    timestamp(_timestamp),
+    product(_product),
+    orderType(_orderType)
+
+    {
+/*       price = _price;
+      amount = _amount;
+      timestamp = _timestamp;
+      product = _product;
+      orderType = _orderType; */
+    }
+
+    double price;
+    double amount;
+    std::string timestamp;
+    std::string product;
+    OrderBookType orderType;
+};
 
 void printMenu()
 
@@ -94,11 +127,55 @@ void processUserOption(int userOption)
 
 int main()
 {
-  while (true)
-  {
-      printMenu();
-      int userOption = getUserOption();
-      processUserOption(userOption);
-  }
-  return 0;
+
+  //double price = 5319.450228;
+  //double amount = 0.00020075;
+  //std::string timestamp{"2020/03/17 17:01:24.884492"};
+  //std:string product("BTC/USDT");
+  //OrderBookType orderType = OrderBookType::ask;
+
+/*   std::vector<double> prices;
+  std::vector<double> amounts;
+  std::vector<std::string> timestamps;
+  std::vector<std::string> products;
+  std::vector<OrderBookType> orderTypes;
+
+  prices.push_back(5000.01);
+  amounts.push_back(0.001);
+  timestamps.push_back("2020/03/17 17:01:24.884492");
+  products.push_back("BTC/USDT");
+  orderTypes.push_back(OrderBookType::bid);
+
+  prices.push_back(10.01);
+  amounts.push_back(0.001);
+  timestamps.push_back("2020/03/17 17:01:24.884492");
+  products.push_back("BTC/USDT");
+  orderTypes.push_back(OrderBookType::ask);
+
+  std::cout << "prices: " << prices[0] <<std::endl;
+  std::cout << "prices: " << prices[1] << std::endl;
+ */
+  /* while (true)
+{
+  printMenu();
+  int userOption = getUserOption();
+  processUserOption(userOption);
+} */
+  //return 0;
+
+  std::vector<OrderBookEntry> orders;
+
+  orders.push_back( OrderBookEntry {1000,
+                        0.02,
+                        "2020/03/17 17:01:24.884492",
+                        "BTC/USDT",
+                        OrderBookType::bid} );
+
+  orders.push_back(OrderBookEntry{2000,
+                                  0.02,
+                                  "2020/03/17 17:01:24.884492",
+                                  "BTC/USDT",
+                                  OrderBookType::bid});
+
+  std::cout << "The price is " << orders[1].price << std::endl;
 }
